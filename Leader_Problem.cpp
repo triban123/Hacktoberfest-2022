c42eff8097d78bda586457ea5a5234f91afd3093
Sahil Rawat <66208562+sahilrawat001@users.noreply.github.com>
2022-10-07 04:58:51 -0700

    Create Leader_Problem.cpp

---
+++ Leader_Problem.cpp
@@ -0,0 +1,18 @@
+
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+        // Code here
+                vector<int> res;
+        int mx = INT_MIN;
+        for(int i = n-1; i >= 0; i--) {
+            mx = max(mx, a[i]);
+            if(mx <= a[i])
+                res.push_back(a[i]);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+
+    }
+};
