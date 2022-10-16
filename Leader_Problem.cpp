bb9a6d9d76adde0f43d9d5be8e6efe757b99fd5e
Avnish Raj <55361674+Devil-9@users.noreply.github.com>
2022-10-16 15:44:19 +0530

    Update Leader_Problem.cpp

--- Leader_Problem.cpp
+++ Leader_Problem.cpp
@@ -16,3 +16,20 @@ class Solution{
 
     }
 };
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> sta(int a[], int n){
+        // Code here
+                vector<int> res;
+        int mx = INT_MIN;
+        for(int i = n-1; i >= 0; i--) {
+            mx = max(mx, a[i]);
+            if(mx <= a[i])
+                res.push_back(a[i]);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+
+    }
+};
